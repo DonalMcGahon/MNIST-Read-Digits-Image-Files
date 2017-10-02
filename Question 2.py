bee3c48947a0956f1dab7078aafeab1bb950b5ab
DonalMcGahon <dmcgahon1234@hotmail.com>
2017-10-02 22:58:36 +0100

    Question2 Compelete

--- Question 2.py
+++ Question 2.py
@@ -8,22 +8,46 @@ index = random.randrange(0, len(images))
 print(mndata.display(images[index]))'''
 
 import gzip
+import io
 
-# Reading in the gz files
-# with gzip.open('Data/t10k-labels-idx1-ubyte.gz', 'rb') as f:
-def read_labels_from_file(filename):
-    with gzip.open(filename, 'rb') as f:
+def read_images_from_file(file):
+    with gzip.open(file, 'rb') as f:
         magic = f.read(4)
         magic = int.from_bytes(magic, 'big')
-        print("Magic is:", magic)
-        nolab = f.read(4)
-        nolab = int.from_bytes(nolab, 'big')
-        print("Number of labels is:", nolab)
+        print("magic is: ", magic)
         
-        labels = [f.read(1) for i in range(nolab)]
-        labels = [int.from_bytes(label, 'big') for label in labels]
+        noimg = f.read(4)
+        noimg = int.from_bytes(noimg, 'big')
+        print("number of labels: ", noimg)
 
-return labels
+        norow = f.read(4)
+        norow = int.from_bytes(norow, 'big')
+        print("number of rows: ", norow)
+        
+        nocol = f.read(4)
+        nocol = int.from_bytes(nocol, 'big')
+        print("number of cols: ", nocol)    
+        
+        
+        images = []
+        
+        for i in range(noimg):
+            rows = []
+            for r in range(norow):
+                cols = []
+                for c in range(nocol):
+                    cols.append(int.from_bytes(f.read(1), 'big'))
+                rows.append(cols)
+            images.append(rows)
+            
+        return images
+    
+    
 
-train_labels = read_labels_from_file('Data/train-labels-idx1-ubyte.gz')
-test_labels = read_labels_from_file('Data/t10k-labels-idx1-ubyte.gz')
+            
+train_images = read_images_from_file('data/train-images-idx3-ubyte.gz')
+
+for row in train_images[4999]:
+    for col in row:
+        print('.' if col <= 127 else '#', end='')
+    print()
